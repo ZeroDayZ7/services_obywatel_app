d3de4c9691554d4d9a9f99456d807f87241b3a81
ZeroDayZ7 <ZeroDayZ7@proton.me>
2026-05-01 12:10:17 +0200

    delete comments

--- platform/services/version-service/config/config.go
+++ platform/services/version-service/config/config.go
@@ -15,16 +15,14 @@ type VersionConfig struct {
 }
 
 func Get() VersionConfig {
-	// Ustawienie pliku konfiguracyjnego
-	viper.SetConfigFile(".env") // lub "config.yaml" jeśli wolisz YAML
-	viper.AutomaticEnv()        // pozwala nadpisać zmienne z systemu
 
-	// Wczytanie pliku
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
 	if err := viper.ReadInConfig(); err != nil {
 		log.Println("Warning: config file not found, using env variables only")
 	}
 
-	// Domyślne wartości
 	viper.SetDefault("VERSION_SERVICE_PORT", "3005")
 	viper.SetDefault("MIN_VERSION", "0.0.0")
 	viper.SetDefault("LATEST_VERSION", "0.0.0")
