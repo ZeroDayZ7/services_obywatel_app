aab532886b539d2e961ad80d16a5c63a26f78e90
ZeroDayZ7 <ZeroDayZ7@proton.me>
2026-08-16 15:32:59 +0200

    refactor(container): remove HealthHandler and related service from container

--- platform/services/officer-bff/internal/di/container.go
+++ platform/services/officer-bff/internal/di/container.go
@@ -8,20 +8,16 @@ import (
 
 type Container struct {
 	Config          *config.Config
-	HealthHandler   *handler.HealthHandler
 	OfficialHandler *handler.OfficialHandler
 }
 
 func BuildContainer(app *config.App) *Container {
-	healthSvc := service.NewHealthService()
 	officialSvc := service.NewOfficialService()
 
-	healthHdl := handler.NewHealthHandler(healthSvc)
 	officialHdl := handler.NewOfficialHandler(officialSvc)
 
 	return &Container{
 		Config:          app.Config,
-		HealthHandler:   healthHdl,
 		OfficialHandler: officialHdl,
 	}
 }
