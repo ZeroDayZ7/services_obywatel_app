b73f2d4985f42fee4f070cb9f86c28ee042af887
ZeroDayZ7 <ZeroDayZ7@proton.me>
2026-08-30 16:27:30 +0200

    feat(kms-admin): add group role creation and permissions for KMS users

--- platform/docker/scripts/init-kms-admin.sh
+++ platform/docker/scripts/init-kms-admin.sh
@@ -4,9 +4,10 @@ set -e
 KMS_USER="${KMS_ADMIN_USER:-kms_admin}"
 KMS_PASSWORD=$(cat /run/secrets/kms_admin_pass)
 TARGET_DB="${POSTGRES_DB:-auth_database}"
+TARGET_ROLE="kms_auth-service_postgres_auth"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$TARGET_DB" <<-EOSQL
-    -- 1. Tworzenie użytkownika kms_admin z prawem CREATEROLE
+    -- 1. Tworzenie użytkownika kms_admin z prawem CREATEROLE oraz roli grupowej
     DO \$block\$
     BEGIN
         IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$KMS_USER') THEN
@@ -14,20 +15,36 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$TARGET_DB" <<-EOS
         ELSE
             EXECUTE format('ALTER USER %I WITH CREATEROLE PASSWORD %L', '$KMS_USER', '$KMS_PASSWORD');
         END IF;
+
+        -- Tworzenie roli grupowej, do której KMS będzie przypisywał tymczasowych userów
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$TARGET_ROLE') THEN
+            EXECUTE format('CREATE ROLE %I NOLOGIN', '$TARGET_ROLE');
+        END IF;
     END
     \$block\$;
 
-    -- 2. Nadanie praw do bazy danych
+    -- 2. Nadanie praw do bazy danych dla KMS
     GRANT ALL PRIVILEGES ON DATABASE "$TARGET_DB" TO "$KMS_USER";
 
-    -- 3. Nadanie praw do schematu public i istniejących tabel
+    -- 3. Nadanie praw do schematu public i istniejących tabel dla KMS
     GRANT ALL ON SCHEMA public TO "$KMS_USER";
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "$KMS_USER";
     GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO "$KMS_USER";
 
-    -- 4. Nadanie domyślnych praw na przyszłe tabele i sekwencje
+    -- 4. Nadanie domyślnych praw na przyszłe tabele i sekwencje dla KMS
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO "$KMS_USER";
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO "$KMS_USER";
+
+    -- 5. KONFIGURACJA ROLI DLA AGENTÓW:
+    -- Nadanie uprawnień tabelarycznych dla samej roli grupowej
+    GRANT USAGE ON SCHEMA public TO "$TARGET_ROLE";
+    GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "$TARGET_ROLE";
+    
+    -- Automatyczne uprawnienia dla nowych tabel tworzonych w przyszłości
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "$TARGET_ROLE";
+
+    -- 6. KLUCZOWE: Pozwól użytkownikowi KMS nadawać tę rolę innym użytkownikom (ADMIN OPTION)
+    GRANT "$TARGET_ROLE" TO "$KMS_USER" WITH ADMIN OPTION;
 EOSQL
 
-echo "Użytkownik $KMS_USER został pomyślnie skonfigurowany w bazie $TARGET_DB."
+echo "Użytkownik $KMS_USER oraz rola $TARGET_ROLE zostały pomyślnie skonfigurowane w bazie $TARGET_DB."
